26d4a99b210bb776b772f39ca6f8561cd51758cc
Ayush Naithani <70189069+ayushnaithani6@users.noreply.github.com>
2021-05-24 11:44:22 +0530

    Update checkCyclicInDG.cpp

--- Graphs/checkCyclicInDG.cpp
+++ Graphs/checkCyclicInDG.cpp
@@ -1,92 +1,40 @@
+// implementation of directed graph cycle check
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std;  
 
-class Graph {
-    public:
-        int v;
-        vector<int> *adjList;
-
-        Graph(int v) {
-            this->v = v;
-            adjList = new vector<int>[v];
-        }
-
-        void addEdge(int u, int v) {
-            adjList[u].push_back(v);
-        }
-};
-
-// function declarations
-Graph* createGraph(void);
-void printGraph(Graph*);
-bool detectCycle(Graph*);
-bool detectCycleUtil(int , vector<int>*&, vector<bool>&, vector<bool>&, int);
-
+bool dfs(vector<int> *&adj, vector<bool> &ok, vector<bool> &dfsOk, int u) {
+     ok[u] = true;
+     dfsOk[u] = true;
+     for(int i: adj[u]) {
+          if(dfsOk[i] == true) {
+               return true;
+          }
+          // if not visited then visit that node if cycle is detected 
+          // while furthur dfs stop then n there and return 
+          else if(!ok[i] && dfs(adj, ok, dfsOk, i)) return true;
+     }
+     dfsOk[u] = false;
+     return false;
+}
 
 int main() {
-    Graph *g = createGraph();
-    // printGraph(g);
-    cout << detectCycle(g);
-    return 0;
-}
+     int V, E;
+     cin >> V >> E;
+     vector<int> *adj = new vector<int>[V];
+     int u, v;
+     while(E--) {
+          cin >> u >> v;
+          adj[u].push_back(v);
+     }
 
-bool detectCycle(Graph *g) {
-    int v = g->v;
-    vector<int> *adjList = g->adjList;
-    // taking visited and storing ancestor in this case
-    vector<bool> ancestor(v, false);
-    vector<bool> visited(v, false);
-    for(int i=0; i<v; ++i) {
-        if(detectCycleUtil(v, adjList, visited, ancestor, i)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool detectCycleUtil(int v, vector<int> *&adjList, vector<bool> &visited, vector<bool> &ancestor, int src) {
-
-    if(!visited[src]) {
-        visited[src] = true;
-        ancestor[src] = true;
-
-        for(int connectedNode: adjList[src]) {
-            if(ancestor[connectedNode]) {
-                return true;
-            }
-            else {
-                if(detectCycleUtil(v, adjList, visited, ancestor, connectedNode)) {
-                    return true;
-                }
-            }
-        }
-    }
-    ancestor[src] = false;
-    return false;
-}
-
-
-Graph* createGraph() {
-    Graph *g = new Graph(5);
-    // we have cycle in this graph
-    g->addEdge(0, 1); 
-    g->addEdge(0, 2); 
-    g->addEdge(2, 3); 
-    g->addEdge(3, 4); 
-    g->addEdge(4, 3);
-    
-    return g;
-}
-
-void printGraph(Graph *g) {
-    int vertices = g->v;
-    vector<int> *adjacencyList = g->adjList;
-
-    for(int i=0; i<vertices; ++i) {
-        cout << i << " -> ";
-        for(int j=0; j<adjacencyList[i].size(); ++j) {
-            cout <<  adjacencyList[i][j] << " ";
-        }
-        cout << endl;
-    }
+     vector<bool> ok(V, false);
+     vector<bool> dfsOk(V, false);
+     for(int i=0; i<V; ++i) {
+          if(!ok[i] && dfs(adj, ok, dfsOk, i)) {
+               cout << "Cycle Detected :(";
+               return 0;
+          }
+     }
+     cout << "Cycle not found :)";
+     return 0;
 }
